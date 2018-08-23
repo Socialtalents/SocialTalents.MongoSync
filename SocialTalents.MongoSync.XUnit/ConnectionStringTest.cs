ad8cf2a42a70078b39f44001d6bcf2d3d05a1f8e
Oleksii Bespalov <alexb5dh@gmail.com>
2018-08-23 19:25:38 +0300

    Switched to built-in MongoDB connection string parsing & added support for replica set

--- SocialTalents.MongoSync.XUnit/ConnectionStringTest.cs
+++ SocialTalents.MongoSync.XUnit/ConnectionStringTest.cs
@@ -1,28 +1,34 @@
 ﻿using SocialTalents.MongoSync.Console.Model;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace SocialTalents.MongoSync.XUnit
 {
     public class ConnectionStringTest
     {
+        // mongo.exe requires database name as first parameter, in this way it is easier to fix parameters
         [Theory]
-        // mongo.exe requires databasename as first paramater, in this way it is easier to fix parameters
-        [InlineData("mongodb://masterDev:BfkvYB@127.0.0.1:28017/master", "127.0.0.1:28017", "masterDev", "BfkvYB", "master",
-            "--db master --host 127.0.0.1:28017 --username masterDev --password BfkvYB")]
-        [InlineData("mongodb://masterDev:BfkvYB@127.0.0.1/master", "127.0.0.1", "masterDev", "BfkvYB", "master",
-            "--db master --host 127.0.0.1 --username masterDev --password BfkvYB")]
-        [InlineData("mongodb://masterDev@127.0.0.1:28017/master", "127.0.0.1:28017", "masterDev", null, "master",
+        [InlineData("mongodb://masterDev:BfkvYB@127.0.0.1/master", new []{"127.0.0.1:27017"}, "masterDev", "BfkvYB", "master",
+            "--db master --host 127.0.0.1:27017 --username masterDev --password BfkvYB")]
+        [InlineData("mongodb://masterDev@127.0.0.1:28017/master", new []{"127.0.0.1:28017"}, "masterDev", null, "master",
             "--db master --host 127.0.0.1:28017 --username masterDev")]
-        [InlineData("mongodb://127.0.0.1:28017/master", "127.0.0.1:28017", null, null, "master"
+        [InlineData("mongodb://127.0.0.1:28017/master", new []{"127.0.0.1:28017"}, null, null, "master"
             , "--db master --host 127.0.0.1:28017")]
-        [InlineData("masterDev:BfkvYB@127.0.0.1:28017/master", "127.0.0.1:28017", "masterDev", "BfkvYB", "master"
+        [InlineData("mongodb://masterDev:BfkvYB@127.0.0.1:28017/master", new []{"127.0.0.1:28017"}, "masterDev", "BfkvYB", "master"
             , "--db master --host 127.0.0.1:28017 --username masterDev --password BfkvYB")]
-        public void TestParsing(string input, string host, string user, string password, string database, string expectedCommandLine)
+        // takes first host from the list if not replica set specified:
+        [InlineData("mongodb://127.0.0.1:28018,127.0.0.1:28019/master", new []{"127.0.0.1:28018", "127.0.0.1:28019"}, null, null, "master"
+            , "--db master --host 127.0.0.1:28018")]
+        // parses replica set:
+        [InlineData("mongodb://127.0.0.1:28018,127.0.0.1:28019/master?replicaSet=rs0", new []{"127.0.0.1:28018", "127.0.0.1:28019"}, null, null, "master"
+            , "--db master --host rs0/127.0.0.1:28018,127.0.0.1:28019")]
+        public void TestParsing(string input, string[] hosts, string user, string password, string database, string expectedCommandLine)
         {
             var c = new ConnectionString(input);
 
-            Assert.Equal(host, c.Host);
+            Assert.Equal(hosts, c.Hosts);
             Assert.Equal(user, c.UserName);
             Assert.Equal(password, c.Password);
             Assert.Equal(database, c.Database);
